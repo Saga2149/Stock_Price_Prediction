fecff86bbdddff64e28352105ebd4523d9b9d1b8
rciphertext <ravindra.singhpawar11@gmail.com>
2019-04-18 22:40:21 +0530

    <ravindra:code updated views.py>

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Timestamp(models.Model):
+    timeStamp = models.DateField()
+
+    def __str__(self):
+        return self.timeStamp.strftime("%Y-%m-%d %H:%M:%S")
